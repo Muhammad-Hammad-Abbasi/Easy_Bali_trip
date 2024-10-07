5d936e8c0bc865716c78d3b1ec3458a7cc3d9259
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-10-07 01:01:42 -0700

    update

--- app/video/video.tsx
+++ app/video/video.tsx
@@ -6,7 +6,7 @@ export default function Video() {
             {/* video adjust */}
             <div>
                 <div>
-                    <video controls autoPlay loop width="1350 " className="min-w-full">
+                    <video controls autoPlay loop width="1450 " className="min-w-full">
                         <source src="/EBT-Vid-Sound.mp4" type="video/mp4" />
                     </video>
                 </div>
